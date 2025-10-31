879073a762f561bc6457d93686a0b5d24efea43b
Maneesh Babu <maneeshbabumedepudi@gmail.com>
2025-10-31 13:57:37 +0530

    Update settings for production configuration
    Set DEBUG to False for production readiness and added Whitenoise for static files management.

--- genshin_advisor/settings.py
+++ genshin_advisor/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vg8yz2i8=rk&u66@+k5=l=9i($v%@y2ds%7g&#s6se=&9lx%fh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -97,7 +97,7 @@ STATIC_URL = '/static/'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'api/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
